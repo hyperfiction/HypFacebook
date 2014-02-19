8acc6f61be2dc6a87ef83882de845b8680530588
Skyler Parr <skyler@tachaeon.com>
2014-02-19 09:52:45 -0800

    added latest FB headers

--- project/iPhone/include/FBAccessTokenData.h
+++ project/iPhone/include/FBAccessTokenData.h
@@ -15,101 +15,126 @@
  */
 
 #import <Foundation/Foundation.h>
+
 #import "FBSession.h"
 
 /*!
  @class FBAccessTokenData
- 
+
  @abstract Represents an access token used for the Facebook login flow
-  and includes associated metadata such as expiration date and permissions.
-  You should use factory methods (createToken...) to construct instances
-  and should be treated as immutable.
- 
- @discussion For more information, see 
+ and includes associated metadata such as expiration date and permissions.
+ You should use factory methods (createToken...) to construct instances
+ and should be treated as immutable.
+
+ @discussion For more information, see
  https://developers.facebook.com/docs/concepts/login/access-tokens-and-types/.
-*/
+ */
 @interface FBAccessTokenData : NSObject <NSCopying>
 
 /*!
  @method
- 
+
  @abstract Creates an FBAccessTokenData from an App Link provided by the Facebook application
-  or nil if the url is not valid.
- 
+ or nil if the url is not valid.
+
  @param url The url provided.
  @param appID needed in order to verify URL format.
  @param urlSchemeSuffix needed in order to verify URL format.
- 
-*/
-+ (FBAccessTokenData *) createTokenFromFacebookURL:(NSURL *)url appID:(NSString *)appID urlSchemeSuffix:(NSString *)urlSchemeSuffix;
+
+ */
++ (FBAccessTokenData *)createTokenFromFacebookURL:(NSURL *)url appID:(NSString *)appID urlSchemeSuffix:(NSString *)urlSchemeSuffix;
 
 /*!
  @method
- 
+
  @abstract Creates an FBAccessTokenData from a dictionary or returns nil if required data is missing.
  @param dictionary the dictionary with FBSessionTokenCachingStrategy keys.
  */
-+ (FBAccessTokenData *) createTokenFromDictionary:(NSDictionary *)dictionary;
++ (FBAccessTokenData *)createTokenFromDictionary:(NSDictionary *)dictionary;
 
 /*!
  @method
- 
+
  @abstract Creates an FBAccessTokenData from existing information or returns nil if required data is missing.
- 
+
  @param accessToken The token string. If nil or empty, this method will return nil.
  @param permissions The permissions set. A value of nil indicates basic permissions.
  @param expirationDate The expiration date. A value of nil defaults to `[NSDate distantFuture]`.
  @param loginType The login source of the token.
  @param refreshDate The date that token was last refreshed. A value of nil defaults to `[NSDate date]`.
  */
-+ (FBAccessTokenData *) createTokenFromString:(NSString *)accessToken
-                                  permissions:(NSArray *)permissions
-                               expirationDate:(NSDate *)expirationDate
-                                    loginType:(FBSessionLoginType)loginType
-                                  refreshDate:(NSDate *)refreshDate;
++ (FBAccessTokenData *)createTokenFromString:(NSString *)accessToken
+                                 permissions:(NSArray *)permissions
+                              expirationDate:(NSDate *)expirationDate
+                                   loginType:(FBSessionLoginType)loginType
+                                 refreshDate:(NSDate *)refreshDate;
 
 /*!
  @method
- 
+
+ @abstract Creates an FBAccessTokenData from existing information or returns nil if required data is missing.
+
+ @param accessToken The token string. If nil or empty, this method will return nil.
+ @param permissions The permissions set. A value of nil indicates basic permissions.
+ @param expirationDate The expiration date. A value of nil defaults to `[NSDate distantFuture]`.
+ @param loginType The login source of the token.
+ @param refreshDate The date that token was last refreshed. A value of nil defaults to `[NSDate date]`.
+ @param permissionsRefreshDate The date the permissions were last refreshed. A value of nil defaults to `[NSDate distantPast]`.
+ */
++ (FBAccessTokenData *)createTokenFromString:(NSString *)accessToken
+                                 permissions:(NSArray *)permissions
+                              expirationDate:(NSDate *)expirationDate
+                                   loginType:(FBSessionLoginType)loginType
+                                 refreshDate:(NSDate *)refreshDate
+                      permissionsRefreshDate:(NSDate *)permissionsRefreshDate;
+
+/*!
+ @method
+
  @abstract Returns a dictionary representation of this instance.
- 
+
  @discussion This is provided for backwards compatibility with previous
  access token related APIs that used a NSDictionary (see `FBSessionTokenCachingStrategy`).
-*/
-- (NSMutableDictionary *) dictionary;
+ */
+- (NSMutableDictionary *)dictionary;
 
 /*!
  @method
- 
+
  @abstract Returns a Boolean value that indicates whether a given object is an FBAccessTokenData object and exactly equal the receiver.
- 
+
  @param accessTokenData the data to compare to the receiver.
-*/
-- (BOOL) isEqualToAccessTokenData:(FBAccessTokenData *)accessTokenData;
+ */
+- (BOOL)isEqualToAccessTokenData:(FBAccessTokenData *)accessTokenData;
 
 /*!
  @abstract returns the access token NSString.
-*/
+ */
 @property (readonly, nonatomic, copy) NSString *accessToken;
 
 /*!
  @abstract returns the permissions associated with the access token.
-*/
+ */
 @property (readonly, nonatomic, copy) NSArray *permissions;
 
 /*!
  @abstract returns the expiration date of the access token.
-*/
+ */
 @property (readonly, nonatomic, copy) NSDate *expirationDate;
 
 /*!
  @abstract returns the login type associated with the token.
-*/
+ */
 @property (readonly, nonatomic) FBSessionLoginType loginType;
 
 /*!
  @abstract returns the date the token was last refreshed.
-*/
+ */
 @property (readonly, nonatomic, copy) NSDate *refreshDate;
+
+/*!
+ @abstract returns the date the permissions were last refreshed.
+ */
+@property (readonly, nonatomic, copy) NSDate *permissionsRefreshDate;
 
 @end
--- project/iPhone/include/FBLoginView.h
+++ project/iPhone/include/FBLoginView.h
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,27 +15,28 @@
  */
 
 #import <UIKit/UIKit.h>
-#import "FBSession.h"
+
 #import "FBGraphUser.h"
+#import "FBSession.h"
 
 @protocol FBLoginViewDelegate;
 
 /*!
  @class FBLoginView
  @abstract FBLoginView is a custom UIView that renders a button to login or logout based on the
-  state of `FBSession.activeSession`
- 
+ state of `FBSession.activeSession`
+
  @discussion This view is closely associated with `FBSession.activeSession`. Upon initialization,
-  it will attempt to open an active session without UI if the current active session is not open.
- 
-  The FBLoginView instance also monitors for changes to the active session.
+ it will attempt to open an active session without UI if the current active session is not open.
+
+ The FBLoginView instance also monitors for changes to the active session.
  */
 @interface FBLoginView : UIView
 
 /*!
  @abstract
  The permissions to login with.  Defaults to nil, meaning basic permissions.
- 
+
  @discussion Methods and properties that specify permissions without a read or publish
  qualification are deprecated; use of a read-qualified or publish-qualified alternative is preferred.
  */
@@ -43,8 +44,9 @@
 
 /*!
  @abstract
- The read permissions to request if the user logs in via this view.
- 
+ The read permissions to request if the user logs in via this view. The basic_info permission must be explicitly requested at
+ first login, and is no longer inferred, (subject to an active migration.)
+
  @discussion
  Note, that if read permissions are specified, then publish permissions should not be specified.
  */
@@ -53,7 +55,7 @@
 /*!
  @abstract
  The publish permissions to request if the user logs in via this view.
- 
+
  @discussion
  Note, that a defaultAudience value of FBSessionDefaultAudienceOnlyMe, FBSessionDefaultAudienceEveryone, or
  FBSessionDefaultAudienceFriends should be set if publish permissions are specified. Additionally, when publish
@@ -67,54 +69,62 @@
  */
 @property (nonatomic, assign) FBSessionDefaultAudience defaultAudience;
 
+/*!
+ @abstract
+ The login behavior for the active session if the user logs in via this view
+
+ @discussion
+ The default value is FBSessionLoginBehaviorUseSystemAccountIfPresent.
+ */
+@property (nonatomic) FBSessionLoginBehavior loginBehavior;
 
 /*!
  @abstract
  Initializes and returns an `FBLoginView` object.  The underlying session has basic permissions granted to it.
  */
-- (id)init;
+- (instancetype)init;
 
 /*!
  @method
- 
+
  @abstract
  Initializes and returns an `FBLoginView` object constructed with the specified permissions.
- 
+
  @param permissions  An array of strings representing the permissions to request during the
- authentication flow. A value of nil will indicates basic permissions. 
- 
+ authentication flow. A value of nil will indicates basic permissions.
+
  @discussion Methods and properties that specify permissions without a read or publish
  qualification are deprecated; use of a read-qualified or publish-qualified alternative is preferred.
  */
-- (id)initWithPermissions:(NSArray *)permissions __attribute__((deprecated));
+- (instancetype)initWithPermissions:(NSArray *)permissions __attribute__((deprecated));
 
 /*!
  @method
- 
+
  @abstract
  Initializes and returns an `FBLoginView` object constructed with the specified permissions.
- 
+
  @param readPermissions  An array of strings representing the read permissions to request during the
  authentication flow. A value of nil will indicates basic permissions.
- 
+
  */
-- (id)initWithReadPermissions:(NSArray *)readPermissions;
+- (instancetype)initWithReadPermissions:(NSArray *)readPermissions;
 
 /*!
  @method
- 
+
  @abstract
  Initializes and returns an `FBLoginView` object constructed with the specified permissions.
- 
+
  @param publishPermissions  An array of strings representing the publish permissions to request during the
- authentication flow. 
- 
+ authentication flow.
+
  @param defaultAudience  An audience for published posts; note that FBSessionDefaultAudienceNone is not valid
  for permission requests that include publish or manage permissions.
- 
+
  */
-- (id)initWithPublishPermissions:(NSArray *)publishPermissions
-                 defaultAudience:(FBSessionDefaultAudience)defaultAudience;
+- (instancetype)initWithPublishPermissions:(NSArray *)publishPermissions
+                           defaultAudience:(FBSessionDefaultAudience)defaultAudience;
 
 /*!
  @abstract
@@ -125,10 +135,10 @@
 @end
 
 /*!
- @protocol 
- 
+ @protocol
+
  @abstract
- The `FBLoginViewDelegate` protocol defines the methods used to receive event 
+ The `FBLoginViewDelegate` protocol defines the methods used to receive event
  notifications from `FBLoginView` objects.
  */
 @protocol FBLoginViewDelegate <NSObject>
@@ -138,7 +148,7 @@
 /*!
  @abstract
  Tells the delegate that the view is now in logged in mode
- 
+
  @param loginView   The login view that transitioned its view mode
  */
 - (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView;
@@ -148,7 +158,7 @@
  Tells the delegate that the view is has now fetched user info
 
  @param loginView   The login view that transitioned its view mode
- 
+
  @param user        The user info object describing the logged in user
  */
 - (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
@@ -157,7 +167,7 @@
 /*!
  @abstract
  Tells the delegate that the view is now in logged out mode
- 
+
  @param loginView   The login view that transitioned its view mode
  */
 - (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView;
@@ -165,7 +175,7 @@
 /*!
  @abstract
  Tells the delegate that there is a communication or authorization error.
- 
+
  @param loginView           The login view that transitioned its view mode
  @param error               An error object containing details of the error.
  @discussion See https://developers.facebook.com/docs/technical-guides/iossdk/errors/
